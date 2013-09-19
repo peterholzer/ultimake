7b7069afc83554da52c2d669c9f285fce54e5e67
Peter Holzer <spam@holzer-direct.de>
2013-09-19 16:01:37 +0200

    See changelog at end of file

--- ultimake.mk
+++ ultimake.mk
@@ -1,249 +1,259 @@
 # Author: Peter Holzer
-# Ultimake v1.09
-# 04.07.2013
-MAKEFILENAME := ultimake-1.09.mk
+# Ultimake v1.10
+# 19.09.2013
+# MAKEFILENAME := ultimake-1.10.mk
+
+
+
+
+.SUFFIXES:
+
+# name of THIS makefile
+THIS := $(notdir $(lastword $(MAKEFILE_LIST)))
+# path of THIS makefile
+HERE := $(realpath $(dir $(lastword $(MAKEFILE_LIST))))
+
+
+#   @echo 'Dieses Makefile kompiliert alle *.cpp und *.c-Files in einem Ordner und '
+#   @echo 'seinen Unterordnern und linkt diese zu einer Anwendung bzw. archiviert  '
+#   @echo 'sie zu einer statischen Bibliothek, je nachdem, welche Daten angegeben  '
+#   @echo 'werden.                                                                 '
+#   @echo 'Je nachdem, ob man eine Anwendung oder eine Bibliothek erstellen möchte,'
+#   @echo 'kann man die jeweils andere Variable einfach leer lassen.               '
+
+
+# @echo 'Aufruf: make -f $(MAKEFILENAME)'
+
+
+
+#   @echo 'Ultimake debug info'
+#   @echo 'SUBDIRS_OUT:               $(SUBDIRS_OUT)'
+#   @echo 'COMPILE.c:                 $(COMPILE.c)'
+#   @echo 'COMPILE.cc:                $(COMPILE.cc)'
+
+
+#=======================================================================
+
+
+# Input related ========================================================
+
+# Search for C/C++ source code files if not specified
+
+## list of C sources
+#ifndef C_SRC
+#    C_SRC   := $(wildcard *.c)
+#    C_SRC   += $(foreach DIR, $(SUBDIRS), $(wildcard $(DIR)/*.c))
+#endif
 #
-# Dieses Makefile kompiliert alle *.cpp und *.c-Files in einem Ordner und seinen Unterordnern
-# und linkt diese zu einer Anwendung bzw. archiviert sie zu einer statischen Bibliothek,
-# je nachdem, welche Daten angegeben werden.
-# Je nachdem, ob man eine Anwendung oder eine Bibliothek erstellen
-# möchte, kann man die jeweils andere Variable einfach leer lassen.
+## list of C++ sources
+#ifndef CXX_SRC
+#    CXX_SRC := $(wildcard *.cpp)
+#    CXX_SRC += $(foreach DIR, $(SUBDIRS), $(wildcard $(DIR)/*.cpp))
+#endif
 
-# Es wird für jedes Source-File ein Makefile mit den Abhängigkeiten erstellt (*.d).
-# Es ist von sämtlichen Headern und Sourcen abhängig, die included werden.
+# TODO: FIND statt WILDCARD?
+# TODO: keine Module/Subdir mehr nötig
 
+C_SRC = $(shell find -name '*.c')
+CXX_SRC = $(shell find -name '*.cpp')
 
-# Targets:
-#     all
-#     clean
-#
-#     debuginfo
-#     doxygen       Dokumentation erstellen, benötigt $(DOXYFILE)
-#     lint          Statische Quellcodeanalyse für C-Files
-#     cppcheck      Statische Quellcodeanalyse für C++-Files
-#     run           Programm ausführen
-#
-#
-# ======================================================================
+#    lua/luaconfig.cpp
+# -> ../out/lua/luaconfig.cpp.o
 
 
-#   BIN             Name für das fertige Programm, falls ein Programm erstellt werden soll
-#   BIN             Name für das fertige Programm
-#   CC              zu benützender C-Compiler
-#   CXX             zu benützender C++-Compiler
-#   CPPFLAGS        Präprozessor Flags
-#   CFLAGS          C-Compiler Flags
-#   CXXFLAGS        C++-Compiler Flags
-#   TARGET_ARCH
+# Output related =======================================================
 
-
-
-
-
-#   Tools:
-#       AR          Archiver
-#       CC          C-Compiler
-#       CXX         C++-Compiler
-#       RM          remove
-#       DOXYGEN     doxygen
-#       LINT        lint
-#
-#   Flags:
-#       CPPFLAGS
-#       CFLAGS
-#       CXXFLAGS
-#       LDFLAGS
-#
-#
-#   CSRC    C-Quelltexte
-#   CXXSRC  C++
-#   OBJ
-#   DEP
-
-
-
-
-
-
-
-
-
-
-
-
-
-# HDR    := $(wildcard *.h) $(wildcard *.hpp)
-
-# Wenn C-Quelldateien NICHT manuell angegeben wurden, danach suchen
-ifndef CSRC
-    CSRC   := $(wildcard *.c)
-    CSRC   += $(foreach DIR, $(SUBDIRS), $(wildcard $(DIR)/*.c))
-endif
-
-# Wenn C++-Quelldateien nicht manuell angegeben wurden, danach suchen
-ifndef CXXSRC
-    CXXSRC := $(wildcard *.cpp)
-    CXXSRC += $(foreach DIR, $(SUBDIRS), $(wildcard $(DIR)/*.cpp))
+# default value
+ifndef OUT
+    OUT  := .
 endif
 
 
+# create path to object and dependency file for every source file
+# add output folder prefix
+# list of dependency files
+DEP := $(addprefix $(OUT)/,$(C_SRC:%.c=%.c.d) $(CXX_SRC:%.cpp=%.cpp.d))
+# list of object files
+OBJ := $(addprefix $(OUT)/,$(C_SRC:%.c=%.c.o) $(CXX_SRC:%.cpp=%.cpp.o))
 
 
-# Output directory präfix anhängen
+
+# add output folder prefix
 ifdef BIN
-    BIN := $(OUT_DIR)/$(BIN)
+    BIN := $(OUT)/$(BIN)
 endif
 ifdef LIB
-    LIB := $(OUT_DIR)/$(LIB)
+    LIB := $(OUT)/$(LIB)
 endif
 
 
-DEP := $(addprefix $(OUT_DIR)/,$(CSRC:%.c=%.c.d) $(CXXSRC:%.cpp=%.cpp.d))
-OBJ := $(addprefix $(OUT_DIR)/,$(CSRC:%.c=%.c.o) $(CXXSRC:%.cpp=%.cpp.o))
+# Tools ================================================================
 
-SUBDIRS_OUT := $(foreach DIR, $(SUBDIRS), $(OUT_DIR)/$(DIR))
-
-$(shell mkdir -p -v $(SUBDIRS_OUT))
-# include the description for each module
-# include $(patsubst %,%/Makefile,$(SUBDIRS))
-
-
-# Standard Tools
-ifndef LINT
-	LINT := splint
+ifndef MKDIR
+    MKDIR := $(shell which mkdir) -p -v
 endif
-ifndef DOXYGEN
-	DOXYGEN := doxygen
+ifndef RM
+    RM := $(shell which rm) -f
 endif
 
 
+# TODO: this
+#ools ::
+#	@$(RM)    --version >/dev/null 2>/dev/null || echo 'rm not found!'
+#	@$(MKDIR) --version >/dev/null 2>/dev/null || echo 'mkdir not found!'
+#	@$(CC)    --version >/dev/null 2>/dev/null || echo 'gcc not found!'
+#	@$(CXX)   --version >/dev/null 2>/dev/null || echo 'g++ not found!'
+#	@$(VALAC) --version >/dev/null 2>/dev/null || echo 'valac not found!'
+
+#	@$() --version >/dev/null 2>/dev/null || echo ' not found!'
 
 
+
+
+
+
+
+
+# Targets ================================================================
 
 all : $(BIN) $(LIB)
-# all: debuginfo
 
-include $(DEP)
+-include $(DEP)
+
+
+clean :
+	@echo 'Cleaning ...'
+	-$(RM) $(BIN) $(LIB) $(OBJ) $(DEP)
+
+
+help ::
+	@echo '                                                            '
+	@echo 'ultimake                                                    '
+	@echo '    $(THIS)'
+	@echo '    $(HERE)'
+	@echo '                                                            '
+	@echo 'Targets:                                                    '
+	@echo '    all        Create binary/static library                 '
+	@echo '    clean      Clean output directory                       '
+	@echo '    help       Show this text                               '
+	@echo '    run        Run executable                               '
+	@echo '                                                            '
+	@echo '    cppcheck                                                '
+	@echo '    doxygen                                                 '
+	@echo '    lint                                                    '
+	@echo '                                                            '
+	@echo '    *.d                                                     '
+	@echo '    *.o                                                     '
+	@echo '                                                            '
+	@echo '                                                            '
+	@echo 'Targets & Output                                            '
+	@echo '    output folder:          $(OUT)'
+	@echo '    target binary     BIN:  $(BIN)'
+	@echo '    target library    LIB:  $(LIB)'
+	@echo '    modules (subfolders):  $(SUBDIRS)'
+	@echo '  '
+	@echo '............................................................'
+	@echo 'Flags:'
+	@echo -e 'CPPFLAGS (preprocessor flags)\n    $(CPPFLAGS:-%=-%\n   )'
+	@echo -e 'CFLAGS (C compiler flags)    \n    $(CFLAGS:-%=-%\n   )'
+	@echo -e 'CXXFLAGS (C++ compiler flags)\n    $(CXXFLAGS:-%=-%\n   )'
+	@echo -e 'LDFLAGS (Linker flags)       \n    $(LDFLAGS:-%=-%\n   )'
+	@echo ' '
+	@echo '............................................................'
+	@echo 'Files:'
+	@echo -e 'C_SRC (C sources)       \n    $(C_SRC:%.c=%.c\n   )'
+	@echo -e 'CXX_SRC (C++ sources)   \n    $(CXX_SRC:%.cpp=%.cpp\n   )'
+	@echo -e 'OBJ (object files)      \n    $(OBJ:%.o=%.o\n   )'
+	@echo -e 'DEP (dependencies)      \n    $(DEP:%.d=%.d\n   )'
+	@echo '  '
+	@echo '  '
+
+
+run: all
+	./$(BIN)
+
+
+.PHONY : all clean help run
+
+.PHONY : tools
+
+
+tools ::
+	@echo '............................................................'
+	@echo 'Tools:'
+	@echo '    archiver         AR:       $(AR)'
+	@echo '    C preprocessor   CPP:      $(CPP)'
+	@echo '    C compiler       CC:       $(CC)'
+	@echo '    C++ compiler     CXX:      $(CXX)'
+#	@echo '    vala compiler    VALAC:    $(VALAC)'
+	@echo '    linker           LD:       $(LD)'
+	@echo '  '
+	@echo '    remove           RM:       $(RM)'
+	@echo '    mkdir            MKDIR:    $(MKDIR)'
+	@echo '  '
 
 
 # Rules ================================================================
 
-help :
-	@echo ' '
-	@echo 'Aufruf: make -f $(MAKEFILENAME)'
-	@echo ' '
-	@echo 'Targets:'
-	@echo '  all        Erstellt alle Ziele'
-	@echo '  clean      Löscht alle Zieldateien'
-	@echo '  cppcheck   '
-	@echo '  debuginfo  Zeigt Variablen des Makefiles an'
-	@echo '  doxygen    Erstellt die Dokumentation'
-	@echo '  help       Zeigt diesen Text an'
-	@echo '  lint       '
-	@echo '  run        '
-	@echo ' '
 
-
-debuginfo :
-	@echo 'target binary    BIN:  $(BIN)'
-	@echo 'target library   LIB:  $(LIB)'
-	@echo ' '
-	@echo 'output folder:'        $(OUT)'
-	@echo 'modules (subfolders):  $(SUBDIRS)'
-	@echo ' '
-	@echo 'Tools:'
-	@echo 'archiver         AR:       $(CC)'
-	@echo 'C compiler       CC:       $(CC)'
-	@echo 'C++ compiler     CXX:      $(CXX)'
-	@echo 'remove           RM:       $(RM)'
-	@echo '                 DOXYGEN:  $(DOXYGEN)'
-	@echo '                 LINT:     $(LINT)'
-	@echo ' '
-	@echo 'Flags:'
-	@echo 'preprocessor     CPPFLAGS: $(CPPFLAGS)'
-	@echo 'C compiler       CFLAGS:   $(CFLAGS)'
-	@echo 'C++ compiler     CXXFLAGS: $(CXXFLAGS)'
-	@echo 'linker           LDFLAGS:  $(LDFLAGS)'
-	@echo ' '
-	@echo 'Files:'
-	@echo -e 'C Sources:    CSRC    \n$(CSRC:%.c=%.c\n )'
-	@echo -e 'C++Sources:   CXXSRC  \n$(CXXSRC:%.cpp=%.cpp\n )'
-	@echo -e 'Objects:      OBJ     \n$(OBJ:%.o=%.o\n )'
-	@echo -e 'Dependencies: DEP     \n$(DEP:%.d=%.d\n )'
-	@echo ' '
-	@echo 'Ultimake debug info'
-	@echo 'SUBDIRS_OUT:               $(SUBDIRS_OUT)'
-	@echo 'COMPILE.c:                 $(COMPILE.c)'
-	@echo 'COMPILE.cc:                $(COMPILE.cc)'
-	@echo ' '
-
-
-doxygen :
-	@echo "Generating documentation"
-	$(DOXYGEN) $(DOXYFILE)
-
-
-
-# C / C++ dependency generation, compilation and linking ===============
-
-
+# Object files =========================================================
 # compile object files from C sources
-# %.o : %.cpp %.d
-$(OUT_DIR)/%.cpp.o : %.cpp $(OUT_DIR)/%.cpp.d
-	@echo -e 'compiling $< \t=> $@'
-	@$(COMPILE.cc) $< -o $@
-
-
-# compile object files from C++ sources
-# %.o : %.c %.d
-$(OUT_DIR)/%.c.o : %.c $(OUT_DIR)/%.c.d
-	@echo -e 'compiling $< \t=> $@'
+$(OUT)/%.c.o : %.c $(OUT)/%.c.d
+#	@$(MKDIR) $(@D)
+	@echo '$(CC) [...] $< -o $@'
 	@$(COMPILE.c) $< -o $@
 
 
+# compile object files from C++ sources
+$(OUT)/%.cpp.o : %.cpp $(OUT)/%.cpp.d
+#	@$(MKDIR) $(@D)
+	@echo '$(CXX) [...] $< -o $@'
+	@$(COMPILE.cc) $< -o $@
+
+
+# Dependency files =====================================================
 # generate dependencies from C sources
-$(OUT_DIR)/%.c.d : %.c
+$(OUT)/%.c.d : %.c
+	@$(MKDIR) $(@D)
 	@echo -e 'creating dependency from $< \t=> $@'
-	@$(CC) -I. -MF"$@" -MG -MM -MP -MT"$@" -MT"$(OUT_DIR)/$(<:%.c=%.c.o)" "$<"
+	@$(CC) -I. -MF"$@" -MG -MM -MP -MT"$@" -MT"$(OUT)/$(<:%.c=%.c.o)" "$<"
 
 
 # generate dependencies from C sources
-$(OUT_DIR)/%.cpp.d : %.cpp
+$(OUT)/%.cpp.d : %.cpp
+	@$(MKDIR) $(@D)
 	@echo -e 'creating dependency from $< \t=> $@'
-	@$(CXX) -I. -std=c++0x -MF"$@" -MG -MM -MP -MT"$@" -MT"$(OUT_DIR)/$(<:%.cpp=%.cpp.o)" "$<"
-#	@$(CXX) -I. -std=c++0x -MF"$@" -MG -MM -MP -MT"$@" -MT"$(OUT_DIR)/$(<:%.cpp=%.cpp.o)" "$<"
+	@$(CXX) -I. -std=c++11 -MF"$@" -MG -MM -MT"$@" -MT"$(OUT)/$(<:%.cpp=%.cpp.o)" "$<"
+
+# Linking ==============================================================
 
 
 # link object files into binary
 $(BIN) : $(OBJ)
+#	@$(MKDIR) $(@D)
 	@echo 'linking $@'
-	@$(LINK.cc) $^ -o $@
+	$(CXX) $(LDFLAGS) $(TARGET_ARCH) $(OBJ)
 
+
+# Archiving ============================================================
 
 # create static library from object files
+# .a : .o
 $(LIB) : $(OBJ)
+#	@$(MKDIR) $(@D)
 	@$(RM) -f $@
 	@echo 'archiving library'
 	@$(AR) $(ARFLAGS) $@ $^
 
 #=======================================================================
-#
-clean :
-	@echo 'Cleaning ...'
-	-$(RM) $(BIN) $(LIB) $(OBJ) $(DEP)
-
-lint :
-	$(LINT) $(CSRC)
-
-cppcheck :
-	cppcheck --enable=all --check-config $(CXXSRC) 2>&1 > /dev/null | sed 's/[][]//g'
 
 
-run: all
-	$(BIN)
 
 
-.PHONY : all clean cppcheck doxygen lint
+
+include $(HERE)/dot.mk
+include $(HERE)/devtools.mk
+
 
 
 
@@ -251,9 +261,16 @@ run: all
 
 # CHANGELOG:
 #
+# v1.10
+#     - refactoring
+#     - replaced make wildcards with shell find
+#     - removed subdirs/modules
+#
 # v1.09
 #     - added logging functionality
 #
 #
 #
 #
+
+
