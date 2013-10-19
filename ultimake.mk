10a820211e7db4365d86a7cab0672c95bb3c1f81
Peter Holzer <spam@holzer-direct.de>
2013-10-19 22:49:44 +0200

    See changelog at end of file

--- ultimake.mk
+++ ultimake.mk
@@ -1,7 +1,69 @@
 # Author: Peter Holzer
-# Ultimake v1.17
-# 18.10.2013
+# Ultimake v1.18
+# 19.10.2013
 
+# compatibility check
+ifdef BIN
+    $(error deprecated Target option BIN defined!)
+endif
+
+# TODO: for some reason, there is absolutely no leading "./" allowed,
+#       otherwise the %-rules wont work
+#       this makes half of the "-not" statements at the creation of the
+#       FILES variable useless
+
+# TODO: new approach for FILE list creation. instead of giving find the location as parameter, cd to the source directory and call "find ."
+# TODO: try to avoid absolute paths and
+# TODO: do not forget about the leading "./" problem
+# TODO: reintroduce OUT_DIR but this time additionally to DEP_DIR and OBJ_DIR
+
+
+
+# Ultimake reads the following macro variables:
+#
+# search folders
+#     SOURCES
+#     INCLUDES
+#
+# generated folders
+#     DEP_DIR
+#     OBJ_DIR
+#     OUT_DIR
+#     VALA_C_DIR
+#     VAPI_DIR
+#
+# generated files
+#     TARGET
+#
+# tools
+#     CC
+#     CXX
+#
+#     VALAC
+#
+#     CP
+#     MKDIR
+#     MV
+#     RM
+
+# ULTIMAKE_NAME
+#
+#
+#
+
+# internal variables
+#     BIN           executable file name without OUT_DIR path
+#     LIB           library    file name without OUT_DIR path
+#
+#     FILES         all files in the SOURCES directories (excludes generated folders)
+#     C_SRC         all C source files in FILES
+#     CXX_SRC       all C++ source files in FILES
+#     VALA_SRC      all Vala source files in FILES
+#
+#     DEP           all dependency files to generate
+#     OBJ           all object files to generate
+#     VALA_C_SRC    all C files to generate from Vala source files
+#     VALA_VAPI     all Vala .vapi files to generate from Vala source files
 
 # Configuration ========================================================
 
@@ -28,7 +90,6 @@ endif
 # .SECONDARY with no prerequisites causes all targets to be treated as secondary
 # (i.e., no target is removed because it is considered intermediate).
 
-# TODO: reintroduce OUT_DIR but this time additionally to DEP_DIR and OBJ_DIR
 
 
 # name of this makefile
@@ -40,46 +101,50 @@ ULTIMAKE_PATH := $(realpath $(dir $(lastword $(MAKEFILE_LIST))))
 
 # CFLAGS += -std=c99
 
-# Default output file, when no BIN or LIB is defined ===================
-ifndef LIB
-    BIN ?= a.out
-endif
+# Default Target  ======================================================
 
+TARGET     ?= a.out
 
 # Default Directories ==================================================
-# TODO: for some reason, there is absolutely no leading "./" allowed,
-#       otherwise the %-rules wont work
-#       this makes half of the "-not" statements at the creation of the
-#       FILES variable useless
+
+
+
+
+
+# default values for include file search directories
+# (will automatically convert to "gcc -I<DIRECTORY>)
+INCLUDES   ?= .
+# default values for source file search directories
+SOURCES    ?= .
+
+# default values for generated directories
+OUT_DIR    ?= debug
 DEP_DIR    ?= .dep
 OBJ_DIR    ?= .obj
-VAPI_DIR   ?= .vala/vapi
 VALA_C_DIR ?= .vala/c
-
+VAPI_DIR   ?= .vala/vapi
 
 # Default Tools ========================================================
 
 VALAC ?= valac
 
+# TODO: CP    ?= cp ...
 MKDIR ?= mkdir -p -v
-MV ?= mv -f
-RM ?= rm -f
-
-# Zusammensetzung der Dateipfade:
-# SOURCES ... OBJ_DIR ... `find ..`
-#
-#    .                        test/test.c
-#    .         /  .obj   /    test/test.o
-#
-#  ~/sync/lib  /         /    test/test.c
-#  ~/sync/lib  /  .obj   /    test/test.o
+MV    ?= mv -f
+RM    ?= rm -f
 
 
-# TODO: new approach for FILE list creation. instead of giving find the location as parameter, cd to the source directory and call "find ."
-# TODO: try to avoid absolute paths and
-# TODO: do not forget about the leading "./" problem
+# Functions ============================================================
 
-# executes find on several directories and allows several exclude paths
+# find
+# executes "find -type f" in several directories
+# usage;
+#     $(call find,$(1))
+# $(1)  search directory(s)
+find = $(foreach dir,$(1), $(shell find $(dir) -type f))
+
+# find_exclude
+# executes find in several directories and allows several exclude paths
 # usage;
 #     $(call find_exclude,$(SEARCH_DIR),$(EXCLUDE_DIR))
 # executes something like:
@@ -87,31 +152,44 @@ RM ?= rm -f
 # $(1)  search directory
 # $(2)  exclude directory
 find_exclude = $(foreach dir,$(1), $(shell find $(dir) -type f $(foreach p,$(2), -not -path "$(p)/*" -not -path "./$(p)/*")))
+# find_exclude = $(foreach dir,$(1), $(shell find $(dir) -type f $(foreach p,$(filter-out .,$(2)), -not -path "$(p)/*" -not -path "./$(p)/*")))
+
+#=======================================================================
+
+
+
+
 
 
 # Create lists of existing files =======================================
-
+# TODO: handle special case when one of the exclude directories is "./"
 # find all files in working directory
 # but exclude all files in DEP_DIR, OBJ_DIR, VAPI_DIR and VALA_C_DIR
 # TODO: fix this leading "./" problem
-FILES := $(call find_exclude,$(SOURCES), $(DEP_DIR) $(OBJ_DIR) $(VAPI_DIR) $(VALA_C_DIR))
+FILES := $(call find,$(SOURCES))
+# FILES := $(call find_exclude,$(SOURCES), $(DEP_DIR) $(OBJ_DIR) $(OUT_DIR) $(VALA_C_DIR) $(VAPI_DIR))
 
 # cut "./" prefix away
 FILES := $(patsubst ./%,%,$(FILES))
 
 # TODO: add description for SOURCES and INCLUDES
-SOURCES := $(foreach dir,$(SOURCES),$(patsubst /%, /%, $(realpath $(dir))))
+# SOURCES := $(foreach dir,$(SOURCES),$(patsubst /%, /%, $(realpath $(dir))))
 
 CPPFLAGS_INC := $(foreach include,$(INCLUDES),-I$(include))
 CPPFLAGS += $(CPPFLAGS_INC)
 
 # filter C/C++/Vala sources
+AS_SRC   ?= $(filter %.S,$(FILES))
 C_SRC    ?= $(filter %.c,$(FILES))
 CXX_SRC  ?= $(filter %.cpp,$(FILES))
 VALA_SRC ?= $(filter %.vala,$(FILES))
 
 
 # Create lists of generated files ======================================
+
+BIN := $(OUT_DIR)/$(TARGET)
+LIB := $(OUT_DIR)/lib$(TARGET).a
+
 
 # create list of vapi files from vala sources
 VALA_VAPI  := $(VALA_SRC:%.vala=$(VAPI_DIR)/%.vapi)
@@ -120,22 +198,31 @@ VALA_VAPI  := $(VALA_SRC:%.vala=$(VAPI_DIR)/%.vapi)
 VALA_C_SRC := $(VALA_SRC:%.vala=$(VALA_C_DIR)/%.c)
 C_SRC += $(VALA_C_SRC)
 
+# create list of dependency and object files from sources
+# and handle folder prefix and file extension
+DEP := $(patsubst %,$(DEP_DIR)/%.dep,$(ASM_SRC) $(C_SRC) $(CXX_SRC))
+OBJ := $(patsubst %,$(OBJ_DIR)/%.o,  $(ASM_SRC) $(C_SRC) $(CXX_SRC))
+
 # create list of dependency files from sources and handle $(DEP_DIR) prefix
-DEP := $(C_SRC:%.c=$(DEP_DIR)/%.c.dep) $(CXX_SRC:%.cpp=$(DEP_DIR)/%.cpp.dep)
+# ASM_DEP := $(ASM_SRC:%.S=$(DEP_DIR)/%.S.dep)
+# C_DEP   := $(C_SRC:%.c=$(DEP_DIR)/%.c.dep)
+# CXX_DEP := $(CXX_SRC:%.cpp=$(DEP_DIR)/%.cpp.dep)
+# DEP := $(ASM_DEP) $(C_DEP) $(CXX_DEP)
 
 # create list of object files from sources and handle $(OBJ_DIR) prefix
-OBJ := $(C_SRC:%.c=$(OBJ_DIR)/%.c.o) $(CXX_SRC:%.cpp=$(OBJ_DIR)/%.cpp.o)
-
-# OBJ := $(foreach sources,$(SOURCES),$(filter $(sources)%,$(C_SRC:sources%.c=$(OBJ_DIR)/%.c.o)))
-# OBJ := $(foreach sources,$(SOURCES),$(patsubst $(sources)/%.c, $(sources)/$(OBJ_DIR)/%.c.o, $(filter $(sources)%,$(C_SRC))))
+# ASM_OBJ := $(ASM_SRC:%.S=$(OBJ_DIR)/%.S.o)
+# C_OBJ   := $(C_SRC:%.c=$(OBJ_DIR)/%.c.o)
+# CXX_OBJ := $(CXX_SRC:%.cpp=$(OBJ_DIR)/%.cpp.o)
+# OBJ := $(ASM_OBJ) $(C_OBJ) $(CXX_OBJ)
 
 # Targets ==============================================================
 
-.PHONY : all clean help help-files help-tools run
+.PHONY : all clean lib run
 
 
-all : $(BIN) $(LIB)
+all : $(BIN)
 
+lib : $(LIB)
 
 clean :
 	@echo 'Cleaning ...'
@@ -155,7 +242,38 @@ run : $(BIN)
 # Rules ================================================================
 
 
+# Dependency files .....................................................
+
+# generate dependency files from assembler source files
+$(DEP_DIR)/%.S.dep : %.S
+	$(AT)$(MKDIR) $(@D)
+	@echo 'creating $@'
+	$(AT)$(CC) $(CPPFLAGS_INC) -MF"$@" -MG -MM -MP -MT"$@" -MT"$(OBJ_DIR)/$(<:%.S=%.S.o)" "$<"
+
+
+# generate dependency files from C source files
+$(DEP_DIR)/%.c.dep : %.c
+	$(AT)$(MKDIR) $(@D)
+	@echo 'creating $@'
+	$(AT)$(CC) $(CPPFLAGS_INC) -MF"$@" -MG -MM -MP -MT"$@" -MT"$(OBJ_DIR)/$(<:%.c=%.c.o)" "$<"
+
+
+# generate dependency files from C source files
+$(DEP_DIR)/%.cpp.dep : %.cpp
+	$(AT)$(MKDIR) $(@D)
+	@echo 'creating $@'
+	$(AT)$(CC) $(CPPFLAGS_INC) -std=c++11 -MF"$@" -MG -MM -MT"$@" -MT"$(OBJ_DIR)/$(<:%.cpp=%.cpp.o)" "$<"
+
+
 # Object files .........................................................
+
+# compile object files from assembler source files
+$(OBJ_DIR)/%.S.o : %.S $(DEP_DIR)/%.S.dep
+	$(AT)$(MKDIR) $(@D)
+	@echo 'creating $@'
+	$(AT)$(COMPILE.S) $< -o $@
+
+
 # compile object files from C source files
 $(OBJ_DIR)/%.c.o : %.c $(DEP_DIR)/%.c.dep
 	$(AT)$(MKDIR) $(@D)
@@ -170,20 +288,12 @@ $(OBJ_DIR)/%.cpp.o : %.cpp $(DEP_DIR)/%.cpp.dep
 	$(AT)$(COMPILE.cc) $< -o $@
 # COMPILE.c  = $(CC) $(CFLAGS) $(CPPFLAGS) $(TARGET_ARCH) -c
 
-# Dependency files .....................................................
-# generate dependencies from C source files
-$(DEP_DIR)/%.c.dep : %.c
-	$(AT)$(MKDIR) $(@D)
+
+# Assembler files ......................................................
+# create assembler files from C source files
+%.s : %.c
+	$(AT)$(CC) $(CPPFLAGS) $(CFLAGS) -C -S $< -o $@
 	@echo 'creating $@'
-	$(AT)$(CC) $(CPPFLAGS_INC) -MF"$@" -MG -MM -MP -MT"$@" -MT"$(OBJ_DIR)/$(<:%.c=%.c.o)" "$<"
-
-
-# generate dependencies from C source files
-$(DEP_DIR)/%.cpp.dep : %.cpp
-	$(AT)$(MKDIR) $(@D)
-	@echo 'creating $@'
-	$(AT)$(CC) $(CPPFLAGS_INC) -std=c++11 -MF"$@" -MG -MM -MT"$@" -MT"$(OBJ_DIR)/$(<:%.cpp=%.cpp.o)" "$<"
-
 
 # Vala .................................................................
 # generate C code from Vala
