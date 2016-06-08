46d7b79642573c136fcaf8c8e45768ff1433780c
Peter Holzer <spam@holzer-direct.de>
2016-06-09 00:58:41 +0200

    Added assembler library to example

---
+++ example/lib/libass/ass.h
@@ -0,0 +1,23 @@
+#ifndef ASS_H___
+#define ASS_H___
+
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+
+/// An example
+void printFunction3(void);
+
+/// Another example
+void printFunction4(void);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif // ASS_H___
