c495d658a0135ebbdc31b7424385ccb64a48f2b3
Peter Holzer <spam@holzer-direct.de>
2016-08-01 02:21:09 +0200

    Removed unused code

--- ultimake-fancy.mk
+++ ultimake-fancy.mk
@@ -21,10 +21,8 @@ endif
         NUM_OF_SOURCES := $(words $(foreach target,$(TARGETS), $($(target).SOURCE_FILES)))
         COUNTER :=
         PROGRESS = $(words $(COUNTER))
-#         PROGRESS := 0
         PROGRESS_FILE := $(OUT_DIR)/ultimake-rebuild-count
         load_progress = $(shell cat $(PROGRESS_FILE) 2> /dev/null || echo "1")
-#         inc_progress  = $(eval PROGRESS := $(shell echo $(PROGRESS)+1 | bc))
         inc_progress  = $(eval COUNTER := x $(COUNTER))
 
     # calculate the percentage of $1 relative to $2, $(call percentage,1,2) -> 50%
@@ -36,20 +34,11 @@ endif
             @printf '$(TERM_CURSOR_UP)$(COLOR_DEP)Scanning dependencies...$(COLOR_NONE) [$(PROGRESS)/$(NUM_OF_SOURCES)]\n';
             @echo -n $(PROGRESS) > $(PROGRESS_FILE);
         endef
-#         define ULTIMAKE.PREDEPENDENCY =
-#             $(inc_progress)
-#             @printf '$(TERM_CURSOR_UP)$(COLOR_DEP)[%3s|%3s|%3s] Scanning dependencies...$(COLOR_NONE)\n' '0' '$(PROGRESS)' '$(NUM_OF_SOURCES)'
-#             @echo -n $(PROGRESS) > $(PROGRESS_FILE);
-#         endef
         ULTIMAKE.POSTDEPENDENCY =
 
         define ULTIMAKE.PRECOMPILE =
              @printf '[%4s] $(COLOR_BUILD)$1$(COLOR_NONE)\n' '$(call percentage,$(PROGRESS),$(load_progress))'
         endef
-#         define ULTIMAKE.PRECOMPILE =
-#              @printf '$(COLOR_BUILD)[%3s|%3s|%3s] $1$(COLOR_NONE)\n' '$(PROGRESS)' '$(load_progress)' '$(NUM_OF_SOURCES)'
-#         endef
-#         ULTIMAKE.POSTCOMPILE = || $(RM) $(@:%.o=%.dep)$(inc_progress) # doesn't work as intended
         ULTIMAKE.POSTCOMPILE = $(inc_progress)
 
         ULTIMAKE.PRELINK  = @printf '$(COLOR_LINK)$1$(COLOR_NONE)\n'
